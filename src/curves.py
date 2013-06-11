dcad4860ed39eaf4230ceb5f0f2ad019739408ea
gidden <matthew.gidden@gmail.com>
2013-06-11 16:59:55 -0500

    provides functionality to graph material inflow and outflow curves as well as material inventory curves. closes #2.

--- src/curves.py
+++ src/curves.py
@@ -2,7 +2,7 @@
 import queries as q
 import helpers as h
 
-def growthCurve(session, fac_t, startMonth, endMonth, inYears = True):
+def growthCurve(session, fac_t, startMonth, endMonth, inYears = False):
     """ returns a list of growth values for a given facility type either per
     month or per year
     """
@@ -11,6 +11,33 @@ def growthCurve(session, fac_t, startMonth, endMonth, inYears = True):
         return [q.nFacs(session, fac_t, year.startMonth, year.endMonth) \
                     for year in years]
     else:
-        return [q.nFacs(session, fac_t, start + i) \
-                    for i in range(end - start + 1)]
+        return [q.nFacs(session, fac_t, startMonth + i) \
+                    for i in range(endMonth - startMonth)]
 
+def materialFlowCurve(session, fac_t, commod_t, startMonth, endMonth, \
+                          direction = "in", inYears = False):
+    """returns a material flow listing into a given facility class and commodity
+    class either by month or by year. the curve is constructed as either "in"
+    flow or "out" flow given by the direction.
+    """
+    if inYears:
+        years = h.getYearPoints(startMonth, endMonth)
+        return [q.materialFlow(session, fac_t, commod_t, \
+                                   year.startMonth, year.endMonth, \
+                                   direction = direction) \
+                    for year in years]
+    else:
+        return [q.materialFlow(session, fac_t, commod_t, \
+                                   startMonth + i, direction = direction) \
+                    for i in range(endMonth - startMonth)]
+
+def materialInventoryCurve(session, fac_t, commod_t, startMonth, endMonth, \
+                               byYear = False):
+    """returns the inventory of materials of a given facility class and
+    commodity class either by month or by year
+    """
+    outof = materialFlowCurve(session, fac_t, commod_t, startMonth, endMonth, \
+                                  direction = "out", byYear = byYear)
+    into = materialFlowCurve(session, fac_t, commod_t, startMonth, endMonth, \
+                                  direction = "in", byYear = byYear)
+    return outof - into
