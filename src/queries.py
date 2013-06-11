9a765ab4fed34860e4d0c9f56e2e930c148bd923
gidden <matthew.gidden@gmail.com>
2013-06-11 16:04:32 -0500

    simplified queries slightly

--- src/queries.py
+++ src/queries.py
@@ -1,37 +1,30 @@
 import tables as tbls
-
 import helpers as h
-
-def qAgents(session, fac_t):
-    """returns a query of entries in the Agents table given the name of an
-    Agent's prototype
-    """
-    search = tbls.Agents.Prototype == fac_t
-    return session.query(tbls.Agents).filter(search)
 
 def nFacs(session, fac_t, startTime, endTime = None):
     """returns the number of agents of a given type at a given time
     """
     if endTime is None: endTime = startTime
 
-    search = tbls.Agents.EnterDate <= endTime
-    entry_rows = qAgents(session, fac_t).filter(search).all()
+    f1 = tbls.Agents.Prototype == fac_t
+    f2 = tbls.Agents.EnterDate <= endTime
+    entry_rows = session.query(tbls.Agents).filter(f1).filter(f2).all()
     entry_ids = set(row.ID for row in entry_rows)
     assert len(entry_rows) == len(entry_ids)
-
-    search1 = tbls.AgentDeaths.DeathDate > startTime
-    search2 = tbls.AgentDeaths.AgentID.in_(entry_ids)
-    result = session.query(tbls.AgentDeaths).filter(search1).filter(search2).all()
+    
+    f1 = tbls.AgentDeaths.DeathDate > startTime
+    f2 = tbls.AgentDeaths.AgentID.in_(entry_ids)
+    result = session.query(tbls.AgentDeaths).filter(f1).filter(f2).all()
     return len(result)
             
 def startMonth(session, simid):
-    search = tbls.SimulationTimeInfo.SimId == simid
-    result = session.query(tbls.SimulationTimeInfo).filter(search).all()
+    f = tbls.SimulationTimeInfo.SimId == simid
+    result = session.query(tbls.SimulationTimeInfo).filter(f).all()
     assert len(result) == 1
     return result[0].SimulationStart
 
 def endMonth(session, simid):
-    search = tbls.SimulationTimeInfo.SimId == simid
-    result = session.query(tbls.SimulationTimeInfo).filter(search).all()
+    f = tbls.SimulationTimeInfo.SimId == simid
+    result = session.query(tbls.SimulationTimeInfo).filter(f).all()
     assert len(result) == 1
     return result[0].Duration
